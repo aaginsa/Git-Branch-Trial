433a7048998563976b334ba371ab76a912b6a40b
aaginsa <aaginsa@gmail.com>
2020-12-02 12:20:37 +0700

    commit2 : modify index.js on cabang-99

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const port = 3000;
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send(' Hello World! ');
+    res.send(' Selamat Sore ');
 });
 app.listen(port, () => console.log(`Listening port ${port}`));
